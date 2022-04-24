92a487e6b44c4f481d75f5255b4151dbd18fee4f
Dev-SardarAbdul <99751976+Dev-SardarAbdul@users.noreply.github.com>
2022-04-24 19:23:15 +0500

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { StyledApp } from "./components/styles/appStyle";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/pages/about";
+import Home from "./components/pages/home";
+import Contact from "./components/pages/contact";
+import Topbar from "./components/navbar/topbar";
+import Footer from "./components/footer/footer";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <StyledApp>
+        <Topbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </StyledApp>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
